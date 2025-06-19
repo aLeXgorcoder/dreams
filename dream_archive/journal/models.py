98da2b12448dee72d4abffed9fe5d190719c0dbb
Aleksey Goryansky <aleksejgoryansky@yandex.ru>
2025-06-19 21:16:00 +0300

    chore(project): создать модель Dream

--- dream_archive/journal/models.py
+++ dream_archive/journal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Dream(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержание')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_modified = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        verbose_name = 'Сон'
+        verbose_name_plural = 'Сны'
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
